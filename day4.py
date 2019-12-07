5add8e19aadb324cbb4d420d2ca945a3475fa057
Tucif <tucif@users.noreply.github.com>
2019-12-07 16:20:36 -0600

    day4 phase 2

--- day4.py
+++ day4.py
@@ -5,51 +5,51 @@ import logging
 def main():
   logging.basicConfig(level=logging.DEBUG)
   inp = input("low, high: ")
-  (low, high) = inp.split(',')
+  (low, high) = inp.split('-')
   low = int(low)
   high = int(high)
   logging.debug(f"{low=},{high=}")
 
-  passwords = find_passwords(low, high)
+  passwords = find_passwords(low, high, True)
   logging.debug(f"{passwords=}")
 
 
-def find_passwords(low,high):
-  highest = 567899
-  lowest = 112345
-  #if high > highest:
-  #  high = highest
-  #if low < lowest:
-  #  low = lowest
-
+def find_passwords(low,high, ignore_larger_groups=False):
   search_space = high-low
   logging.debug(f"{search_space=}")
   matches = 0
   for i in range(high - low + 1):
     candidate = str(low+i)
-    doubles = 0
-    if candidate[0] == candidate[1]:
-      doubles += 1
-    if candidate[1] == candidate[2]:
-      doubles += 1
-    if candidate[2] == candidate[3]:
-      doubles += 1
-    if candidate[3] == candidate[4]:
-      doubles += 1
-    if candidate[4] == candidate[5]:
-      doubles += 1
-  
-    if doubles < 1:
-      continue
+    lastdigit = -1
+    longest_repeated = 0
+    match = True
+    doubles = {}
+    for digit in candidate:
+      digit = int(digit)
 
+      if digit < lastdigit:
+        match = False
+      if digit == lastdigit:
+        longest_repeated += 1
+      else:
+        longest_repeated = 1
 
-    if ((candidate[1] > candidate[0] or candidate[1] == candidate[0]) and
-       (candidate[2] > candidate[1] or candidate[2] == candidate[1]) and
-       (candidate[3] > candidate[2] or candidate[3] == candidate[2]) and
-       (candidate[4] > candidate[3] or candidate[4] == candidate[3]) and
-       (candidate[5] > candidate[4] or candidate[5] == candidate[4]) ):
-      matches += 1
+      if longest_repeated == 2:
+        doubles[digit] = True
+
+      # Phase 2
+      if ignore_larger_groups:
+        if longest_repeated > 2:
+          if digit in doubles:
+            del doubles[digit] 
+
+      lastdigit = digit
+
+    if len(doubles.keys()) < 1:
+      match = False
+    if match:
       logging.debug(f"Match: {candidate=}")
+      matches+=1
 
   return matches
 

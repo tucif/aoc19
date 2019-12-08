4c046e458559e796193eca32f032bcf6ed9b4c34
Tucif <tucif@users.noreply.github.com>
2019-12-07 19:25:53 -0600

    day5 phase 2 in day2.py

--- day2.py
+++ day2.py
@@ -4,14 +4,12 @@ import fileinput
 from itertools import product
 
 def op_sum(arr, pos, pmodes):
-  op_bin(arr, pos, lambda a,b: a+b, pmodes)
-  return 1
+  return op_bin(arr, pos, pmodes, lambda a,b: a+b)
 
 def op_mul(arr,pos,pmodes):
-  op_bin(arr, pos, lambda a,b: a*b, pmodes)
-  return 1
+  return op_bin(arr, pos, pmodes, lambda a,b: a*b)
 
-def op_bin(arr, pos, operation, pmodes):
+def op_bin(arr, pos, pmodes, operation):
   
   if pmodes[0] == 0:
     first_operand_location = arr[pos+1]
@@ -34,9 +32,9 @@ def op_bin(arr, pos, operation, pmodes):
     logging.debug(f"{result_location=} {op_result=}")
   else:
     logging.error(f"Write location parameter in immediate mode")
-    return 0
+    return (0, None)
 
-  return 1
+  return (1, None)
 
 
 def op_inp(arr, pos, pmodes):
@@ -46,8 +44,8 @@ def op_inp(arr, pos, pmodes):
     arr[dest] = inp
   else:
     logging.error(f"Write location parameter in immediate mode")
-    return 0
-  return 1
+    return (0, None)
+  return (1, None)
 
 
 def op_out(arr, pos, pmodes):
@@ -57,12 +55,44 @@ def op_out(arr, pos, pmodes):
   else:
     out = arr[pos+1]
     logging.info(f"OUT: {out}")
-  return 1
+  return (1, None)
 
 
 def op_halt(arr,pos, pmodes):
   logging.info(f"HALT")
-  return None
+  return (0, None)
+
+def _op_jmp_cnd(arr, pos, pmodes, cnd):
+  new_pc = None
+  if pmodes[0] == 0:
+    flag_loc = arr[pos+1]
+    flag = arr[flag_loc]
+  else:
+    flag = arr[pos+1]
+
+  if cnd(flag):
+    if pmodes[1] == 0:
+      new_pc_loc = arr[pos+2]
+      new_pc = arr[new_pc_loc]
+    else:
+      new_pc = arr[pos+2]
+
+  return (1, new_pc)
+
+def op_jnz(arr, pos, pmodes):
+  return _op_jmp_cnd(arr, pos, pmodes, lambda x: x != 0)
+
+
+def op_jz(arr, pos, pmodes):
+  return _op_jmp_cnd(arr, pos, pmodes, lambda x: x == 0)
+
+
+def op_lt(arr, pos, pmodes):
+  return op_bin(arr, pos, pmodes, lambda a,b: 1 if a < b else 0)
+
+
+def op_eq(arr, pos, pmodes):
+  return op_bin(arr, pos, pmodes, lambda a,b: 1 if a == b else 0)
 
 
 OPERATIONS = {
@@ -71,6 +101,10 @@ OPERATIONS = {
                 2: (op_mul, 3),
                 3: (op_inp, 1),
                 4: (op_out, 1),
+                5: (op_jnz, 2),
+                6: (op_jz,  2),
+                7: (op_lt,  3),
+                8: (op_eq,  3),
                99: (op_halt, 0)}
 
 def read_instruction(instruction):
@@ -102,12 +136,16 @@ def intcode(code):
     try:
       logging.debug(f"Processing {opcode=}")
       (operation, num_params) = OPERATIONS[opcode]
-      result = operation(code, pc, param_modes)
+      (result, new_pc) = operation(code, pc, param_modes)
     except KeyError:
       logging.error(f"Invalid operation: {opcode=}")
       return
     if result:
-      pc += (num_params+1)
+      if new_pc:
+        pc = new_pc
+      else:
+        # +1 to move from instruction loc
+        pc += (num_params+1)
     else:
       break
 
@@ -133,7 +171,7 @@ def find_result(target, code):
    
     
 if __name__ == "__main__":
-  logging.basicConfig(level=logging.INFO) 
+  logging.basicConfig(level=logging.DEBUG) 
   for line in fileinput.input():
     code = list(map(int,line.split(',')))
     #operate(code, 12, 2)

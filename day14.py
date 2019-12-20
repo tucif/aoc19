1cd048453da97b8c50289a6b2adc26119f52baad
tucif <tucif@users.noreply.github.com>
2019-12-19 23:05:15 -0600

    Day 14 phase1 refactor to make all batches at once

--- day14.py
+++ day14.py
@@ -4,6 +4,7 @@ import logging
 import re
 from collections import defaultdict
 from io import StringIO
+from math import ceil
 
 class Ingredient(object):
   def __init__(self, name, amount):
@@ -21,6 +22,8 @@ class Chemical(object):
 
   def parse_recipe(self, recipe):
     ingredients = []
+    if not recipe:
+      return ingredients
     components = recipe.split(',')
     for component in components:
       match = re.search('(\d+) (\w+)', component)
@@ -34,10 +37,10 @@ class Chemical(object):
 class NanoFactory(object):
   def __init__(self, reactions):
     self.chemicals = self.parse_reactions(reactions)
-    self.stock = defaultdict(int)
+    self.reset_stock()
 
   def parse_reactions(self, reactions):
-    chemicals = {}
+    chemicals = {'ORE': Chemical('ORE',1,'')}
     production_rules = reactions.rstrip().split('\n')
     for production_rule in production_rules:
       recipe, product = production_rule.split('=>')
@@ -49,39 +52,71 @@ class NanoFactory(object):
   def get_ore_used(self):
     return abs(self.stock['ORE'])
 
+  def reset_stock(self):
+    self.stock = defaultdict(int)
+
   def has_chemical_in_stock(self, name, amount):
     if name == 'ORE':
       return True
     return self.stock[name] >= amount
 
-  def consume_chemical(self, name, amount):
+  def consume_chemical(self, name, amount, indent=1):
     # non ORE shouldn't be allowed below 0
     self.stock[name] -= amount
+    indentation = '\t' * indent
+    logging.debug(f"{indentation}Consumed {amount} of {name}, remaining: {self.stock[name]}")
 
-  def produce_chemical(self, name, amount):
-    while not self.has_chemical_in_stock(name, amount):
-      # produce a batch of {name} until there's enough in stock
-      chemical = self.chemicals.get(name)
+  def produce_chemical(self, name, amount, indent=1):
+    indentation = '\t' * indent
+    chemical = self.chemicals.get(name)
+    stock = self.stock[chemical.name]
+    original_amount = amount
+    if name != 'ORE':
+      # update amount required with stock
+      amount -= stock
+    batches = max(1,ceil(amount / chemical.batch_size))
+    logging.debug(f"{indentation}producing {name}: needs {amount} (after taking {stock} from stock), make {batches=} of {chemical.batch_size} each")
+    if not self.has_chemical_in_stock(name, original_amount):
       for ingredient in chemical.recipe:
-        self.produce_chemical(ingredient.name, ingredient.amount)
-        self.consume_chemical(ingredient.name, ingredient.amount)
-      self.stock[chemical.name] += chemical.batch_size
+        self.produce_chemical(ingredient.name, batches * ingredient.amount, indent+1)
+        self.consume_chemical(ingredient.name, batches * ingredient.amount, indent+1)
+      self.stock[chemical.name] += batches * chemical.batch_size
+    else:
+      logging.debug(f"{indentation}produced {name}: needs {amount=} has {self.stock[name]} in stock")
     
   def __repr__(self):
     return f"NanoFactory({self.chemicals})"
 
+  def max_fuel_with_ore(self, total_ore, ore_per_fuel):
+    # start searching from integer division
+    max_fuel = total_ore // ore_per_fuel
+    self.produce_chemical('FUEL', max_fuel)
+    while (ore_used := self.get_ore_used()) < total_ore:
+      remaining = total_ore - ore_used
+      logging.debug(f"{max_fuel=} {ore_used=}")
+      self.produce_chemical('FUEL', max_fuel)
+      max_fuel +=1
+    return max_fuel
+
+
+
 
 def main():
-  logging.basicConfig(level=logging.DEBUG)
+  logging.basicConfig(level=logging.INFO)
   s = StringIO()
   for line in fileinput.input():
     s.write(line)
   s.seek(0)
   factory = NanoFactory(s.read())
-  factory.produce_chemical('A', 10)
   logging.debug(f"{factory=}")
+  factory.produce_chemical('FUEL', 1)
+  ore_per_fuel = factory.get_ore_used()
   logging.debug(f"{factory.stock=}")
-  logging.info(f"{factory.get_ore_used()}")
+  logging.info(f"{ore_per_fuel=}")
+  factory.reset_stock()
+  one_trillion = 1_000_000_000_000
+  max_fuel = factory.max_fuel_with_ore(one_trillion, ore_per_fuel)
+  logging.info(f"{max_fuel=}")
   
 
 if __name__ == '__main__':
